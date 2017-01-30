883b4e317087e50ab0e56203512e320872b6038c
masa <masa.guruto@gmail.com>
2017-01-31 01:12:52 +0900

    ex: 4.01: getOrElse

--- exercises/src/test/scala/fpinscala/errorhandling/TestErrorHandling.scala
+++ exercises/src/test/scala/fpinscala/errorhandling/TestErrorHandling.scala
@@ -14,4 +14,9 @@ class TestErrorHandling extends FunSuite with Matchers{
     assert(Some(1).map(_ * 5) == Some(5))
     assert((None: Option[Int]).map(_ * 5) == None)
   }
+
+  test("4.01: higher order functions: getOrElse"){
+    assert(Some(1).getOrElse(0) == 1)
+    assert(None.getOrElse(0) == 0)
+  }
 }
